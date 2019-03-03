ed2e025a80219438068e9472e1c2fe2153bcd06f
Qiang Li <liqiang@gmail.com>
2019-03-03 09:50:02 -0800

    terrasa theme

--- create.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-git checkout -b terrassa
-git submodule add https://github.com/danielkvist/hugo-terrassa-theme.git themes/terrassa 
-cp -rf themes/terrassa/exampleSite/content/* content/
-cp -f themes/terrassa/exampleSite/config.toml .
-
-
