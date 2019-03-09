65aedebdaa855827b41a7a46bc7787eaf23ec05f
Qiang Li <liqiang@gmail.com>
2019-03-08 17:24:20 -0800

    terrassa theme

--- create.sh
+++ create.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
 
-git checkout -b terrassa
-git submodule add https://github.com/danielkvist/hugo-terrassa-theme.git themes/terrassa 
+theme=terrassa
+
+#git checkout -b terrassa
+#git submodule add https://github.com/danielkvist/hugo-terrassa-theme.git themes/terrassa 
+git submodule add https://github.com/danielkvist/hugo-terrassa-theme.git terrassa/themes/terrassa
+
 cp -rf themes/terrassa/exampleSite/content/* content/
 cp -f themes/terrassa/exampleSite/config.toml .
 
